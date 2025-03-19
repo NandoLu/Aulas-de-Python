c20cf5c4d34f4c9b2dfd3431113d26163a66775d
Luiz Fernando Balbino <106929467+NandoLu@users.noreply.github.com>
2025-03-19 10:28:39 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,23 @@
+# Lista sobre operadores artiméticos
+x=10; a=5;b=2; c=9; d=20
+
+print("Ex_1: ",x + a / b)
+print("Ex_2: ",x + a // b)
+print("Ex_3: ",(x + a ) % b)
+print("Ex_4: ",d - d * a + c )
+print("Ex_5: ",2 * b % 9 + 7)
+print("Ex_6: ",d - b *( a + c ))
+print("Ex_7: ",(d - b ) * (a + c))
+print("Ex_8: ",x + d % b + a * c * b - a)
+print("Ex_9: ",((x + d) // b)+(a *(c + b - a)))
+
+# Desafio de distancia percorrida
+distancia = int(input("Digite a distancia: "))
+distancia*= 1000
+print("A distancia percorrida de Km corresponde a",distancia,"metros")
+
+# Comandos de entrada e saída em Python 
+a = input("Digite um numero: ")
+b = input("Digite um numero: ")
+print (a+b)
+a = int
