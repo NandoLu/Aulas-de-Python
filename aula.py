f28f55fec3ac5311d4e4fa11b02cd361a76a1dd7
Luiz Fernando Balbino <106929467+NandoLu@users.noreply.github.com>
2025-03-19 11:13:19 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -11,9 +11,10 @@ print("Ex_7: ",(d - b ) * (a + c))
 print("Ex_8: ",x + d % b + a * c * b - a)
 print("Ex_9: ",((x + d) // b)+(a *(c + b - a)))
 
+
 # Desafio de distancia percorrida
 distancia = int(input("Digite a distancia: "))
-print(f"A distancia percorrida de Km corresponde a {distancia*1000} metros")
+print(f"A distancia percorrida de \n Km corresponde a {distancia*1000} metros")
 
 # Comandos de entrada e saída em Python 
 a = input("Digite um numero: ")
@@ -27,3 +28,18 @@ a = int
 booleano = input("É verdade? ").strip().lower() == "sim"
 print(booleano)
 
+# Exercicio com nome
+'''***
+rerwe
+***'''
+
+nome = input("Digite o nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+print(f" Olá, {nome}! \n Você tem {idade} anos \n Sua altura e {altura} metros")
+breakpoint
+
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+n3 = n1 + n2
+print(f"Vamos printar {n1}+{n2} = {n3}")
