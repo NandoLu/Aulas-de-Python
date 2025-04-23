ea47b52c7730e9105a93c47d866553c53831c074
Luiz Fernando Balbino <106929467+NandoLu@users.noreply.github.com>
2025-04-23 12:05:38 -0300

    Add files via upload

--- while.py
+++ while.py
@@ -1,107 +1,125 @@
+# range(stop)
+# range(start, stop)
+# range(start, stop, step)
+
+# quantidade = 0
+# for cont in range(10):
+#     numero = int(input("Digite um número inteiro: "))
+#     if numero%2 == 0:
+#         quantidade +=1
+# match quantidade:
+#     case 0:
+#         print("Você não digitou nenhum número par")
+#     case 1:
+#         print(f"Voce digitou {quantidade} número par")
+#     case _:
+#         print(f"Voce digitou {quantidade} números pares")
+
+# Exercício 1
+
+# usuario = int(input("Digite um numero de um a dez: "))
+
+# while usuario < 1 or usuario >10:
+#     usuario = int(input("Digite um numero de um a dez, otário: "))
+
+# for cont in range(1,11):
+#     if cont == usuario:
+#         print(f"Você digitou {usuario}")
+
+# Exercício 2
+
+# for i in range(10):
+#     if i % 3 == 0:
+#         continue
+#     print(i)
+#     print(i)
+
+
+
+# for i in range(0,10,2):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+# lista = ["a", "e", "i","o","u"]
+
+# for i in range(len(lista)):
+#     print("{0}".format(lista[i]))
+
+# Varrer texto
+
 # cont = 0
-# limite = int(input("Digite até que número voce deseja:"))
-# while cont < limite:
-   
+# texto = "A o s a n o d n a d"
 
+# for caractere in texto:
+#     if caractere != " ":
+#         cont+=1
+# print(cont)
 
-#     cont+=1
-#     print(cont)
+# Cadastro de Aluno:
 
-# print("Acabou, mané")
+nome = input("Digite o nome do aluno:")
+rm = int(input("Digite o RM do aluno:"))
+senha = int(input("Digite a senha do aluno:"))
+senhav = int(input("Digite a senha novamente:"))
+while senhav != senha:
+    senhav = int(input("Senha não coincide, digite novamente:"))
 
-# import os
-# os.system('cls' if os.name == 'nt' else 'clear')
+print("--- Aluno cadastrado! ---")
+print("--- Boletim ---")
 
-# par = 0
-# cont = 0
-# while cont < 10:
-#     num = int(input("Digite o número voce deseja:"))
-#     if num%2 == 0:
-#         par+=1
-#     cont += 1
+rmlog = int(input("Digite o RM do aluno:"))
+while rmlog != rm:
+    rmlog = int(input("RM não encontrado, digite novamente:"))
 
-# print("Acabou, mané")
-# print(f"Voce digitou {par} numeros pares")
+senhalog = int(input("Digite a senha do aluno:"))
 
-# ------------ MEDIA
+cont = 0
+if senhalog != senha:
+    while cont < 3:
+        senhalog = int(input("Senha errada, digite novamente!"))
+        if senhalog == senha:
+            print("Acesso permitido!")
+            break
+        else:
+            cont+=1
+            print(f"Senha errada, {3 - cont} tentativas restante!")
+    if cont == 3:
+        print("Numero de tentativas esgotado!")
+    
+cp1 = float(input("Digite o valor do Checkpoint 1: "))
+while cp1 < 0 or cp1 > 10:
+    cp1 = float(input("A nota deve ser entre 0 e 10, digite novamente: "))
+cp2 = float(input("Digite o valor do Checkpoint 2: "))
+while cp2 < 0 or cp2 > 10:
+    cp2 = float(input("A nota deve ser entre 0 e 10, digite novamente: "))
+cp3 = float(input("Digite o valor do Checkpoint 3: "))
+while cp3 < 0 or cp3 > 10:
+    cp3 = float(input("A nota deve ser entre 0 e 10, digite novamente: "))
 
+cps = 0
+if cp1 < cp2 and cp1 < 3:
+    cps = cp2 + cp3
+elif cp2 < cp1 and cp2 < cp3:
+    cps = cp1 + cp3
+else:
+    cps = cp1 + cp2
 
-# import os
+sprint1 = float(input("Digite o sprint 1: "))
+while sprint1 < 0 or sprint1 > 10:
+    sprint1 = float(input("A nota deve ser entre 0 e 10, digite novamente: "))
 
-# media = float(input("Digite a media: "))
-# while media < 0 or media > 10:
-#     os.system('cls')
-#     print("Media errada, digite novamente")
-#     media = float(input("Digite a media: "))
+sprint2 = float(input("Digite o sprint 2: "))
+while sprint2 < 0 or sprint2 > 10:
+    sprint2 = float(input("A nota deve ser entre 0 e 10, digite novamente: "))
 
-# if media < 6:
-#     print("Reprovado")
-# elif media >= 6 and media <= 7:
-#     print("Aprovado, mas pode melhorar!")
-# else:
-#     print("Aprovado, muito bem!")
+resultado = ((sprint1 + sprint2 + cps)/4)*0.4
+resultado = ((6 - resultado)*10)/6
+print(f"A nota necessária para passar é {resultado:.2f}") 
 
-# ------------ MEDIA
-
-# x = 0
-# y = 0
-
-# while x < 10:
-#     x+=1
-#     y+=x
-# print(f"Somatório dos números é {y}")
-
-# ------------ TABUADA
-
-# x = 0
-# y = int(input("A tabuada:"))
-# while x <= 10:
-#     print(f"{y} x {x} = {x*y}")
-#     x+=1
-
-# -------------- DESAFIO
-
-# Luiz Fernando Balbino
-# RM 566222
-import os
-print("CARDÁPIO: \n 100 Cachorro quente: 13.20 \n 101 Hamburguer: 19.90 \n 102 Cheeseburguer: 21.90 \n 103 Suco Natural: 7.00 \n 104 Refrigerante: 6.50 \n 105 Terminar Compra")
-
-
-cachorro = 13.2
-hamburguer = 19.9
-cheese = 21.9
-suco = 7.0
-refrigerante = 6.5
-sistema = True
-total = 0
-
-
-while sistema == True :
-    os.system('cls')
-
-    print("CARDÁPIO: \n 100 Cachorro quente: 13.20 \n 101 Hamburguer: 19.90 \n 102 Cheeseburguer: 21.90 \n 103 Suco Natural: 7.00 \n 104 Refrigerante: 6.50 \n 105 Terminar Compra")
-
-    produto = int(input("\n Digite o codigo do produto: "))
     
     
-    match produto:
-        case 100:
-            quantidade = int(input("Digite a quantidade: "))
-            total = total + cachorro * quantidade
-        case 101:
-            quantidade = int(input("Digite a quantidade: "))
-            total = total + quantidade * hamburguer
-        case 102:
-            quantidade = int(input("Digite a quantidade: "))
-            total = total + quantidade * cheese
-        case 103:
-            quantidade = int(input("Digite a quantidade: "))
-            total = total + quantidade * suco
-        case 104:
-            quantidade = int(input("Digite a quantidade: "))
-            total = total + quantidade * refrigerante
-        case 105:
-            print(f"O valor total é: {total:.2f}")
-            sistema = False
-        case _:
-            print("Codigo inválido!")
+    
+        
+
